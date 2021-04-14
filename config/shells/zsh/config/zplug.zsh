8f89321561281fdbffbde7a9e58c38407f170cf6
llosimura <alfonso@turo.com>
2021-04-13 18:47:39 -0700

    feat: extract zplug config

---
+++ config/shells/zsh/config/zplug.zsh
@@ -0,0 +1,37 @@
+source ~/.zplug/init.zsh
+
+# Load theme
+zplug romkatv/powerlevel10k, as:theme
+
+# Plugins
+zplug plugins/aws, from:oh-my-zsh
+zplug plugins/docker, from:oh-my-zsh
+zplug plugins/fzf, from:oh-my-zsh
+zplug Aloxaf/fzf-tab, from:github
+zplug plugins/git, from:oh-my-zsh
+zplug plugins/git-extras, from:oh-my-zsh
+zplug plugins/node, from:oh-my-zsh
+zplug plugins/npm, from:oh-my-zsh
+zplug plugins/osx, from:oh-my-zsh
+zplug plugins/z, from:oh-my-zsh
+zplug plugins/vi-mode, from:oh-my-zsh
+zplug plugins/terraform, from:oh-my-zsh
+zplug lukechilds/zsh-nvm
+zplug zsh-users/zsh-syntax-highlighting
+zplug zsh-users/zsh-autosuggestions
+zplug zsh-users/zsh-completions
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+   printf "Install missing plugins? [y/N]:"
+   if read -q; then
+      echo; zplug install
+   fi
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load
+
+# Load the completions
+autoload -Uz compinit && compinit
+
