2687002ba46cf84917abd2c5c394f0486514c9a2
llosimura <alfonso@turo.com>
2021-04-17 10:53:24 -0700

    refactor(shell): extract common config between bash and zsh

---
+++ config/shells/common/common.sh
@@ -0,0 +1,4 @@
+# shellcheck source=/dev/null
+[[ ! -f ~/.config/shells/common/config/settings.sh ]] || source ~/.config/shells/common/config/settings.sh
+[[ ! -f ~/.config/shells/common/config/aliases.sh ]] || source ~/.config/shells/common/config/aliases.sh
+[[ ! -f ~/.config/shells/common/config/functions.sh ]] || source ~/.config/shells/common/config/functions.sh
