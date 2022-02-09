d1914fd2675f68032c6fe1787ae1afa2b6b64b6e
alfonso gonzalez <alfonso@turo.com>
2022-02-08 17:01:47 -0800

    fix: improve config

--- config/shells/bash/config/fzf.bash
+++ config/shells/bash/config/fzf.bash
@@ -6,8 +6,8 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/Cellar/fzf/0.29.0/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/Cellar/fzf/0.29.0/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/opt/homebrew/Cellar/fzf/0.29.0/shell/key-bindings.zsh"
+source "/opt/homebrew/Cellar/fzf/0.29.0/shell/key-bindings.bash"
--- config/shells/common/config/settings.sh
+++ config/shells/common/config/settings.sh
@@ -24,18 +24,11 @@ export GOPATH=$HOME/go
 # Path configuration
 export PATH="$HOME/.local/bin:$HOME/.yarn/bin:$HOME/bin:$HOME/.jenv/bin:/usr/local/go/bin:$GOPATH/bin:$HOME/flutter/bin:$PATH"
 
-# Confiure jenv
-eval "$(jenv init -)"
-
-# Configure pyenv
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
-
 # Configure FZF
 # Do not use full screen mode
 export FZF_DEFAULT_OPTS='--height 50% --reverse'
 # Use ripgrep instead of grep
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
-
+## Configure
+export GPG_TTY=$(tty)
