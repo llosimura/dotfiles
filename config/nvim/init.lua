64c552af063eb244ace7bd607c0425524169553c
alfonso gonzalez <alfonsoglezluis@gmail.com>
2023-10-06 13:02:04 -0400

    feat: remove nvim config, use astrovim instead

--- config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require('general.sets')
-require('general.netrw')
-require('general.mappings')
-require('general.colors')
-
-require('plugins')
-require('plugins/airline')
--- require('plugins/treesitter')
--- require('plugins/lsp')
