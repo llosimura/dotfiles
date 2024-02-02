6f6ac2e1b80a8da0068bed4efd3e057bc7ac8f18
alfonso gonzalez <alfonsoglezluis@gmail.com>
2024-02-02 19:25:20 +0000

    feat: config vim theme

--- config/astrovim_user/init.lua
+++ config/astrovim_user/init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "dracula",
+  colorscheme = "catppuccin-macchiato",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
